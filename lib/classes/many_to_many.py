f89dadef35d843b69063ca0431fad7109321c1e1
kinotikm <cloutkevin254@gmail.com>
2024-05-27 17:44:10 +0300

    initial commit

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,8 +1,11 @@
 class Article:
     def __init__(self, author, magazine, title):
+        if not isinstance(title, str) or not (5 <= len(title) <= 50):
+            raise ValueError("Title must be a string and between 5-50 characters")
         self.author = author
         self.magazine = magazine
         self.title = title
+        
         
 class Author:
     def __init__(self, name):
